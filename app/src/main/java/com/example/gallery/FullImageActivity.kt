9418ca17dea73c5d14317a1672fa1cbb078d391d
vilgad <rohitgupta16685@gmail.com>
2021-11-16 16:40:16 +0530

    Applied binding

--- app/src/main/java/com/example/gallery/FullImageActivity.kt
+++ app/src/main/java/com/example/gallery/FullImageActivity.kt
@@ -9,17 +9,16 @@ import android.os.StrictMode.VmPolicy
 import android.os.StrictMode
 import android.content.Intent
 import android.net.Uri
-import android.widget.Button
-import android.widget.ImageView
+import androidx.databinding.DataBindingUtil
+import com.example.gallery.databinding.ActivityFullImageBinding
 
 class FullImageActivity : AppCompatActivity() {
-    var position = 0
-    private lateinit var imageView: ImageView
+    private lateinit var binding: ActivityFullImageBinding
+    private var position = 0
     private lateinit var imageLink: String
-    private lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_full_image)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_full_image)
         ActivityCompat.requestPermissions(
             this,
             arrayOf(permission.READ_EXTERNAL_STORAGE, permission.WRITE_EXTERNAL_STORAGE),
@@ -27,20 +26,17 @@ class FullImageActivity : AppCompatActivity() {
         )
         val builder = VmPolicy.Builder()
         StrictMode.setVmPolicy(builder.build())
-        imageView = findViewById(R.id.fullimageViewid)
-        button = findViewById(R.id.button)
         val bundle = intent.extras
         if (bundle != null) {
             position = bundle.getInt("pos", 0)
             imageLink = bundle.getString("image").toString()
         }
-        imageView.setImageURI(Uri.parse(imageLink))
-        button.setOnClickListener{
+        binding.fullimageViewid.setImageURI(Uri.parse(imageLink))
+        binding.button.setOnClickListener{
             val intent = Intent(Intent.ACTION_SEND)
             //                // putting uri of image to be shared
             intent.putExtra(Intent.EXTRA_STREAM, Uri.parse(imageLink))
             //                // adding text to share
-            intent.putExtra(Intent.EXTRA_TEXT, "Sharing Image")
             //                // Add subject Here
             intent.putExtra(Intent.EXTRA_SUBJECT, "Add Subject")
             //                // setting type to image
