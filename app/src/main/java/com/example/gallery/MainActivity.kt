315f987dd6deeaa1e045b4159a328ae22a6c2e9b
vilgad <rohitgupta16685@gmail.com>
2021-12-29 10:15:35 +0530

    fixed Bottom navigation bug

--- app/src/main/java/com/example/gallery/MainActivity.kt
+++ app/src/main/java/com/example/gallery/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.gallery.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,38 +15,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-//        val navHostFragment =
-//            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
-//        val navController = navHostFragment.navController
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.mainBottomNavigation.setupWithNavController(navController)
 
-        val scrollFrag= ScrollingFragment()
-        val folderFrag= FolderFragment()
-
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.fragmentContainerView,scrollFrag)
-        }
-
-        binding.mainBottomNavigation.setOnItemSelectedListener {
-            when(it.itemId) {
-                R.id.nav_folders -> {
-                    supportFragmentManager.beginTransaction().apply {
-                        replace(R.id.fragmentContainerView,folderFrag)
-                        commit()
-                    }
-                    Log.d(TAG,"folder is selected")
-                    true
-                }
-
-                R.id.nav_photos -> {
-                    supportFragmentManager.beginTransaction().apply {
-                        replace(R.id.fragmentContainerView,scrollFrag)
-                        commit()
-                    }
-                    Log.d(TAG,"photos is selected")
-                    true
-                }
-                else -> false
-            }
-        }
+        // to change the fragment in FragmentContainerView
+//        val scrollFrag= ScrollingFragment()
+//        val folderFrag= FolderFragment()
+//
+//        supportFragmentManager.beginTransaction().apply {
+//            replace(R.id.fragmentContainerView,scrollFrag)
+//        }
+//
+//        binding.mainBottomNavigation.setOnItemSelectedListener {
+//            when(it.itemId) {
+//                R.id.nav_folders -> {
+//                    supportFragmentManager.beginTransaction().apply {
+//                        replace(R.id.fragmentContainerView,folderFrag)
+//                        commit()
+//                    }
+//                    Log.d(TAG,"folder is selected")
+//                    true
+//                }
+//
+//                R.id.nav_photos -> {
+//                    supportFragmentManager.beginTransaction().apply {
+//                        replace(R.id.fragmentContainerView,scrollFrag)
+//                        commit()
+//                    }
+//                    Log.d(TAG,"photos is selected")
+//                    true
+//                }
+//                else -> false
+//            }
+//        }
     }
 }
