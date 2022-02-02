ccd99669924eb4f718d85045e71ddc8d52788af6
GeekLord04 <chiragofficial04@gmail.com>
2022-02-02 22:04:18 +0530

    Splash Screen done

---
+++ app/src/main/java/com/example/gallery/SplashscreenActivity.kt
@@ -0,0 +1,24 @@
+package com.example.gallery
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashscreenActivity : AppCompatActivity() {
+
+    lateinit var handler: Handler
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+
+        handler = Handler()
+        handler.postDelayed({
+
+            // Delay and Start Activity
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        } , 3000) // here we're delaying to startActivity after 3seconds
+    }
+}
