351d09bd4e2381474de9341c85ee6178541ea48e
Vivek <itguru4all@gmail.com>
2022-02-03 21:51:25 +0530

    Fixed Issue #16 App not working on Redmi note 7 pro

--- app/src/main/java/com/example/gallery/ScrollingFragment.kt
+++ app/src/main/java/com/example/gallery/ScrollingFragment.kt
@@ -1,3 +1,5 @@
+
+
 package com.example.gallery
 
 import android.Manifest
@@ -10,9 +12,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
@@ -20,7 +21,7 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.gallery.databinding.FragmentScrollingBinding
-import java.util.ArrayList
+import com.google.android.material.snackbar.Snackbar
 
 
 class ScrollingFragment : Fragment() {
@@ -29,53 +30,119 @@ class ScrollingFragment : Fragment() {
     private lateinit var albumRecyclerViewAdapter: AlbumRecyclerViewAdapter
     private lateinit var images: List<String>
 
-    private val My_READ_PERMISSION_CODE = 101
-    private val CAMERA_PERMISSION_CODE = 993
+
+private val requestPermissionLauncher =
+    registerForActivityResult(ActivityResultContracts.RequestPermission()){
+        isGranted->
+        if(isGranted)
+        {
+            startCamera()
+        }
+        else{
+            Snackbar.make(binding.root,
+            R.string.camera_permission_denied,
+            Snackbar.LENGTH_SHORT).show()
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View?
+    {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_scrolling, container, false)
 
-        if (ContextCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.READ_EXTERNAL_STORAGE
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                requireActivity(),
-                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-                My_READ_PERMISSION_CODE
-            )
-        } else {
-            loadImages()
-        }
-
-        // Camera button for opening camera
-        val cameraButton = binding.cameraFloatingButton
-
-        // OnClickListener for the camera button
-        cameraButton.setOnClickListener {
-            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-
-            // Check if permissions have been granted, if not, then request permissions
-            if(ContextCompat.checkSelfPermission(requireContext(),Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED)
-            {
-                requestPermissions(arrayOf(Manifest.permission.CAMERA),CAMERA_PERMISSION_CODE)
-                return@setOnClickListener
-            }
-
-            // Check if there exists an Activity to handle the intent, if yes, then send the intent for opening camera
-            if(activity?.let { it1 -> intent.resolveActivity(it1.packageManager) } !=null)
-            {
-                startActivity(intent)
-            }
+        binding.cameraFloatingButton.setOnClickListener {
+            showCameraPreview()
         }
 
         return binding.root
     }
 
+    private fun showCameraPreview() {
+
+        if(ContextCompat.checkSelfPermission(requireContext(),Manifest.permission.CAMERA)== PackageManager.PERMISSION_GRANTED){
+            startCamera()
+        }
+        else
+            requestCameraPermission2()
+    }
+
+    private fun requestCameraPermission2() {
+
+        if(shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)){
+            Snackbar.make(binding.root,
+            R.string.permission_required,
+            Snackbar.LENGTH_INDEFINITE).setAction(R.string.ok){
+                requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+            }.show()
+        }else
+        {
+            requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+        }
+
+    }
+
+
+    private val requestReadPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()){
+                isGranted->
+            if(isGranted)
+            {
+                loadImages()
+            }
+            else{
+                Snackbar.make(binding.root,
+                    R.string.read_permission_denied,
+                    Snackbar.LENGTH_SHORT).show()
+            }
+        }
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        showImages()
+    }
+
+    private fun showImages() {
+        if(ContextCompat.checkSelfPermission(requireContext(),Manifest.permission.READ_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED){
+            loadImages()
+        }
+        else
+            requestReadPermission()
+
+    }
+
+    private fun requestReadPermission() {
+        if(shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)){
+            Snackbar.make(binding.root,
+                R.string.permission_required,
+                Snackbar.LENGTH_INDEFINITE).setAction(R.string.ok){
+                requestReadPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+            }.show()
+        }else
+        {
+            requestReadPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+
+
+    }
+
+    private fun startCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        // Check if there exists an Activity to handle the intent, if yes, then send the intent for opening camera
+        if (activity?.let { it1 -> intent.resolveActivity(it1.packageManager) } != null) {
+            startActivity(intent)
+        }
+    }
+
+
+
     private fun loadImages() {
+
+        Log.i("myTag","This method has been called after permission enabled")
         binding.photosRecyclerView.setHasFixedSize(true)
         val gridLayoutManager: RecyclerView.LayoutManager = GridLayoutManager(requireContext(), 3)
         binding.photosRecyclerView.layoutManager = gridLayoutManager
@@ -109,36 +176,6 @@ class ScrollingFragment : Fragment() {
         binding.photosRecyclerView.adapter = photosRecyclerViewAdapter
     }
 
-    private val mPermissionResult = registerForActivityResult(
-        ActivityResultContracts.RequestPermission()
-    ) { result ->
-        if (result) {
-            Toast.makeText(requireContext(), "Permission Granted", Toast.LENGTH_SHORT).show()
-        } else {
-            Toast.makeText(requireContext(), "Permission Denied", Toast.LENGTH_SHORT).show()
-        }
-    }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        if(grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED)
-        {
-            // Check if camera permissions were requested and if they have been granted now
-            if(requestCode==CAMERA_PERMISSION_CODE && ContextCompat.checkSelfPermission(requireContext(),Manifest.permission.CAMERA)== PackageManager.PERMISSION_GRANTED)
-            {
-                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-
-                // Check if there exists an Activity to handle the intent, if yes, then send the intent for opening camera
-                if(activity?.let { intent.resolveActivity(it.packageManager) } !=null)
-                {
-                    startActivity(intent)
-                }
-            }
-        }
-    }
 }
