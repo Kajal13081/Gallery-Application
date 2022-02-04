1d33f1ac7899b51293bf695acbd6cd40bfb9636e
Vivek <itguru4all@gmail.com>
2022-02-05 01:33:17 +0530

    Added Feature #13 Add Sort Feature *Changed Theme to enable Option Menu *Added Sort Feature inside ScrollingFragment.kt *fixed app performance FullImageFragment.kt

--- app/src/main/java/com/example/gallery/ImagesGallery.kt
+++ app/src/main/java/com/example/gallery/ImagesGallery.kt
@@ -6,11 +6,13 @@ import android.net.Uri
 import android.provider.MediaStore
 import java.util.ArrayList
 
+
 object ImagesGallery {
+
+
+
     @JvmStatic
     fun listOfImage(context: Context): ArrayList<String> {
-        val cursor: Cursor?
-        var column_index_folder_name: Int
         val listOfAllImages = ArrayList<String>()
         var absolutePathOfImage: String
         val uri: Uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
@@ -18,19 +20,87 @@ object ImagesGallery {
             MediaStore.MediaColumns.DATA,
             MediaStore.Images.Media.BUCKET_DISPLAY_NAME
         )
-        val orderBy = MediaStore.Video.Media.DATE_TAKEN
-        cursor = context.contentResolver.query(
-            uri, projection, null,
-            null, "$orderBy DESC"
-        )   // cannot use context.contentResolver in fragment
-        val column_index_data: Int = cursor!!.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA)
+
+        val projection1 = arrayOf(
+            MediaStore.MediaColumns.DATA,
+            MediaStore.Images.Media.BUCKET_DISPLAY_NAME,
+        )
+        val orderByDate = MediaStore.Images.Media.DATE_TAKEN + " DESC"
+        val orderByName =   MediaStore.Images.ImageColumns.BUCKET_DISPLAY_NAME + " DESC"
+        val orderByModifiedDate = MediaStore.Images.ImageColumns.DATE_MODIFIED + " DESC"
+
+
+
+        val  cursor1 = context.contentResolver.query(uri,
+            projection1,null, null, orderByName )
+
+        /* val orderBy = MediaStore.Video.Media.DATE_TAKEN
+         cursor = context.contentResolver.query(
+             uri, projection, null,
+             null, "$orderBy DESC"
+         )   // cannot use context.contentResolver in fragment
+
+        */ val column_index_data: Int = cursor1!!.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA)
 
         //get folder name
         //column_index_folder_name = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME);
-        while (cursor.moveToNext()) {
-            absolutePathOfImage = cursor.getString(column_index_data)
+        while (cursor1.moveToNext()) {
+            absolutePathOfImage = cursor1.getString(column_index_data)
             listOfAllImages.add(absolutePathOfImage)
         }
+
         return listOfAllImages
     }
+
+    enum class SortOrder{
+        Name,
+        Date,
+        Modified,
+        Size
+    }
+    @JvmStatic
+    fun listOfSortedImages(context: Context, sortOrder : SortOrder) : ArrayList<String>{
+
+        val listOfAllImages = ArrayList<String>()
+        var absolutePathOfImage: String
+
+        val uri: Uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+
+        val projection1 = arrayOf(
+            MediaStore.MediaColumns.DATA,
+            MediaStore.Images.Media.BUCKET_DISPLAY_NAME,
+        )
+
+        var orderBy : String?
+        when(sortOrder){
+            SortOrder.Date -> {
+                orderBy = MediaStore.Images.Media.DATE_TAKEN + " DESC"
+            }
+            SortOrder.Modified ->{
+                orderBy = MediaStore.Images.ImageColumns.DATE_MODIFIED + " DESC"
+            }
+            SortOrder.Size ->{
+                orderBy = MediaStore.Images.ImageColumns.SIZE + " DESC"
+            }
+            else ->{
+                orderBy = MediaStore.Images.ImageColumns.BUCKET_DISPLAY_NAME + " ASC"
+            }
+        }
+
+        val  cursor1 = context.contentResolver.query(uri,
+            projection1,null, null, orderBy )
+
+        val column_index_data: Int = cursor1!!.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA)
+
+        //get folder name
+        //column_index_folder_name = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME);
+        while (cursor1.moveToNext()) {
+            absolutePathOfImage = cursor1.getString(column_index_data)
+            listOfAllImages.add(absolutePathOfImage)
+        }
+
+        return listOfAllImages
+
+    }
+
 }
