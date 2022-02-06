e28ece086a9ad48dba7b5d7e9b0b98aa435af89f
Vivek <itguru4all@gmail.com>
2022-02-06 23:18:38 +0530

    Just simplified share Issue!

--- app/src/main/java/com/example/gallery/FullImageFragment.kt
+++ app/src/main/java/com/example/gallery/FullImageFragment.kt
@@ -19,6 +19,9 @@ import android.view.WindowManager
 import android.widget.ImageView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.FileProvider
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
@@ -37,8 +40,6 @@ class FullImageFragment : Fragment() {
     private lateinit var binding: FragmentFullImageBinding
     private var position = 0
     private lateinit var imageLink: String
-
-    private var fileUri : Uri? = null
     private var barsHidden : Boolean = false
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -56,13 +57,6 @@ class FullImageFragment : Fragment() {
             PackageManager.PERMISSION_GRANTED
         )
 
-
-//        val bundle = requireActivity().intent.extras        // cant use intent directly in fragment
-//        if (bundle != null) {
-//            position = bundle.getInt("pos", 0)
-//            imageLink = bundle.getString("image").toString()
-//        }
-
         // hiding bottom navigation bar
         (activity as MainActivity).hideBottomNavBar(true)
 
@@ -71,31 +65,21 @@ class FullImageFragment : Fragment() {
         position = args.pos
         imageLink = args.img
 
-
         binding.fullimageViewid.setImageURI(Uri.parse(imageLink))
 
         binding.bottomNavigation.setOnItemSelectedListener {
             val intent = Intent(Intent.ACTION_SEND)
 
-            // putting uri of image to be shared
-            //this image link must be accessible by other apps( see permission in AndroidManifest.xml inside provider tag)
-/*
-Explanation of Coding...
-*       you have selected a file from media Gallery
-*You have a Uri from that file.
-* But when you want to launch share intent, you have to pass a FileProvider Uri into the intent.
-* But you can’t use the file Uri you have from Media folder. It is not under your app authority
-* and it is not under your app’s file path. If you want to open the file, you have to copy the
-*  file under your app’s file path. And get that copy file Uri with FileProvider and pass it to the intent
-*
- */
+            val file = File(Uri.parse(imageLink).path)
+            val uri = FileProvider.getUriForFile(requireContext(),
+                "com.codepath.fileprovider",
+                file)
+            intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
 
-
-
-            intent.putExtra(Intent.EXTRA_STREAM,fileUri )
+            intent.putExtra(Intent.EXTRA_STREAM,uri )
             //                // adding text to share
             //                // Add subject Here
-            intent.putExtra(Intent.EXTRA_SUBJECT, "Add Subject")
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Shared using Snap!!")
             //                // setting type to image
             intent.type = "image/*"
             //                // calling startActivity() to share
@@ -112,7 +96,19 @@ Explanation of Coding...
             if(barsHidden)
             {
                 bottomBar.animate().translationY(0F)
+
+               //as Flag is deprecated
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                    activity?.window?.let { WindowCompat.setDecorFitsSystemWindows(it, false) }
+                    val windowInsetsController =
+                        activity?.window?.let { ViewCompat.getWindowInsetsController(it.decorView) }
+                            ?: return@setOnClickListener
+                    // Hide both the status bar and the navigation bar
+                    windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+                }
+                else
                 (activity as MainActivity).window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
                 barsHidden = false
             }
 
@@ -120,7 +116,20 @@ Explanation of Coding...
             else
             {
                 bottomBar.animate().translationY(bottomBar.getHeight().toFloat())
+
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+                    activity?.window?.let {
+                        WindowCompat.setDecorFitsSystemWindows(it,true)
+                    }
+                    val windowInsetsController =
+                        activity?.window?.let { ViewCompat.getWindowInsetsController(it.decorView) } ?:return@setOnClickListener
+                    // show both the status bar and the navigation bar
+                    windowInsetsController.show(WindowInsetsCompat.Type.systemBars())
+
+                }
+                else
                 (activity as MainActivity).window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
                 barsHidden = true;
             }
         }
@@ -130,74 +139,4 @@ Explanation of Coding...
     }
 
 
-    override fun onDestroy() {
-        super.onDestroy()
-
-        //Once the fragment gets destroyed , we do not need the file created earlier using method getBitmapFromDrawable()
-        //So, removing the file (We don't wanna increase app's user data storage for every share by user)
-        if (fileUri != null) {
-            context?.contentResolver?.delete(fileUri!!,null,null)
-        }
-
-    }
-
-
-
-    //As we need to give external app permission to access our uri, we need to create a local file and get the uri from there .
-    /**
-     * This method extract bitmap from imageview and return Uri accessible by any app.
-     * We are saving files to (/Android/data/[packagename]/files)
-     * Storage Call : Context.getExternalFilesDir() = data can be read/write by the app,
-     * any apps granted with READ_STORAGE permission can read too, deleted when uninstalled (/Android/data/[packagename]/files)
-     * @param imageview imageview from which we need to extract bitmap
-     * @return Uri uri of the image extracted from imageview
-     */
-    private fun getBitmapFromDrawable(imageview: ImageView): Uri? {
-
-        //get the drawable
-        val drawable = imageview.drawable
-
-        //Extract Bitmap from Imageview drawable
-        var bmp: Bitmap? = if (drawable is BitmapDrawable) {
-            (imageview.getDrawable() as BitmapDrawable).bitmap
-        } else {
-            Log.d("myTag","Drawable is null")
-            return null
-        }
-
-        //Store image to default external storage directory
-        var bmpUri : Uri? = null
-
-        try{
-            val file = File(requireActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES),
-                "share_image_" + System.currentTimeMillis() + ".png")
-            val out = FileOutputStream(file)
-
-
-            bmp?.compress(Bitmap.CompressFormat.JPEG, 80, out)
-            out.close()
-
-            //as per documentation, Uri.fromFile(file) will fail for api>=24, using file provider instead
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N ) {
-                bmpUri = FileProvider.getUriForFile(requireActivity(),
-                    "com.codepath.fileprovider", file)
-            }
-            else
-            {
-                bmpUri = Uri.fromFile(file)
-            }
-        }catch (e : IOException){
-            e.message
-        }
-        return bmpUri
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        CoroutineScope(Dispatchers.IO).launch {
-            fileUri = getBitmapFromDrawable(binding.fullimageViewid)
-        }
-
-    }
 }
